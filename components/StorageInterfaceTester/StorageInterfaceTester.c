de61a1b3072f71d6ef3a97de34645d6eb374d9c4
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.com>
2020-09-22 13:52:33 +0200

    [TMP] Disable testing other storages
    Testing other storages is not relevant for porting to Sabre platform.

--- components/StorageInterfaceTester/StorageInterfaceTester.c
+++ components/StorageInterfaceTester/StorageInterfaceTester.c
@@ -8,71 +8,71 @@ int run()
 {
     const Storage_t storagesUnderTest[] =
     {
-        // ramDisk
-        {
-            .interface =
-            {
-                .write        = ramDisk_rpc_write,
-                .read         = ramDisk_rpc_read,
-                .erase        = ramDisk_rpc_erase,
-                .getSize      = ramDisk_rpc_getSize,
-                .getBlockSize = ramDisk_rpc_getBlockSize,
-                .getState     = ramDisk_rpc_getState
-            },
-            .port = OS_DATAPORT_ASSIGN(ramDisk_port)
-        },
-        // chanMuxStorage
-        {
-            .interface =
-            {
-                .write        = chanMuxStorage_rpc_write,
-                .read         = chanMuxStorage_rpc_read,
-                .erase        = chanMuxStorage_rpc_erase,
-                .getSize      = chanMuxStorage_rpc_getSize,
-                .getBlockSize = chanMuxStorage_rpc_getBlockSize,
-                .getState     = chanMuxStorage_rpc_getState
-            },
-            .port = OS_DATAPORT_ASSIGN(chanMuxStorage_port)
-        },
-        // storageServer 1
-        {
-            .interface =
-            {
-                .write        = storageServer1_rpc_write,
-                .read         = storageServer1_rpc_read,
-                .erase        = storageServer1_rpc_erase,
-                .getSize      = storageServer1_rpc_getSize,
-                .getBlockSize = storageServer1_rpc_getBlockSize,
-                .getState     = storageServer1_rpc_getState
-            },
-            .port = OS_DATAPORT_ASSIGN(storageServer1_port)
-        },
-        // storageServer 2
-        {
-            .interface =
-            {
-                .write        = storageServer2_rpc_write,
-                .read         = storageServer2_rpc_read,
-                .erase        = storageServer2_rpc_erase,
-                .getSize      = storageServer2_rpc_getSize,
-                .getBlockSize = storageServer2_rpc_getBlockSize,
-                .getState     = storageServer2_rpc_getState
-            },
-            .port = OS_DATAPORT_ASSIGN(storageServer2_port)
-        },
-        // storageServer 3
-        {
-            .interface =
-            {
-                .write        = storageServer3_rpc_write,
-                .read         = storageServer3_rpc_read,
-                .erase        = storageServer3_rpc_erase,
-                .getSize      = storageServer3_rpc_getSize,
-                .getBlockSize = storageServer3_rpc_getBlockSize,
-                .getState     = storageServer3_rpc_getState
-            },
-            .port = OS_DATAPORT_ASSIGN(storageServer3_port)
-        },
+        // // ramDisk
+        // {
+        //     .interface =
+        //     {
+        //         .write        = ramDisk_rpc_write,
+        //         .read         = ramDisk_rpc_read,
+        //         .erase        = ramDisk_rpc_erase,
+        //         .getSize      = ramDisk_rpc_getSize,
+        //         .getBlockSize = ramDisk_rpc_getBlockSize,
+        //         .getState     = ramDisk_rpc_getState
+        //     },
+        //     .port = OS_DATAPORT_ASSIGN(ramDisk_port)
+        // },
+        // // chanMuxStorage
+        // {
+        //     .interface =
+        //     {
+        //         .write        = chanMuxStorage_rpc_write,
+        //         .read         = chanMuxStorage_rpc_read,
+        //         .erase        = chanMuxStorage_rpc_erase,
+        //         .getSize      = chanMuxStorage_rpc_getSize,
+        //         .getBlockSize = chanMuxStorage_rpc_getBlockSize,
+        //         .getState     = chanMuxStorage_rpc_getState
+        //     },
+        //     .port = OS_DATAPORT_ASSIGN(chanMuxStorage_port)
+        // },
+        // // storageServer 1
+        // {
+        //     .interface =
+        //     {
+        //         .write        = storageServer1_rpc_write,
+        //         .read         = storageServer1_rpc_read,
+        //         .erase        = storageServer1_rpc_erase,
+        //         .getSize      = storageServer1_rpc_getSize,
+        //         .getBlockSize = storageServer1_rpc_getBlockSize,
+        //         .getState     = storageServer1_rpc_getState
+        //     },
+        //     .port = OS_DATAPORT_ASSIGN(storageServer1_port)
+        // },
+        // // storageServer 2
+        // {
+        //     .interface =
+        //     {
+        //         .write        = storageServer2_rpc_write,
+        //         .read         = storageServer2_rpc_read,
+        //         .erase        = storageServer2_rpc_erase,
+        //         .getSize      = storageServer2_rpc_getSize,
+        //         .getBlockSize = storageServer2_rpc_getBlockSize,
+        //         .getState     = storageServer2_rpc_getState
+        //     },
+        //     .port = OS_DATAPORT_ASSIGN(storageServer2_port)
+        // },
+        // // storageServer 3
+        // {
+        //     .interface =
+        //     {
+        //         .write        = storageServer3_rpc_write,
+        //         .read         = storageServer3_rpc_read,
+        //         .erase        = storageServer3_rpc_erase,
+        //         .getSize      = storageServer3_rpc_getSize,
+        //         .getBlockSize = storageServer3_rpc_getBlockSize,
+        //         .getState     = storageServer3_rpc_getState
+        //     },
+        //     .port = OS_DATAPORT_ASSIGN(storageServer3_port)
+        // },
         // sdHostController
         {
             .interface =
