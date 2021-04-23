7950c526e67b99c2c935d92ac1bb67378e246fac
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-04-23 10:01:36 +0200

    [FIX] Mark variable as unused for RELEASE builds

--- components/StorageInterfaceTester/StorageInterfaceTester.c
+++ components/StorageInterfaceTester/StorageInterfaceTester.c
@@ -5,6 +5,7 @@
  *
  */
 #include "test_storage.h"
+#include "lib_compiler/compiler.h"
 #include "lib_debug/Debug.h"
 #include "SysLoggerClient.h"
 
@@ -14,7 +15,7 @@
 
 int run()
 {
-    OS_Error_t err = SysLoggerClient_init(sysLogger_Rpc_log);
+    DECL_UNUSED_VAR(OS_Error_t err) = SysLoggerClient_init(sysLogger_Rpc_log);
     Debug_ASSERT(err == OS_SUCCESS);
 
     uint32_t stateBitmap = 0;
